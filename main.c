76a3536d28a1b876160c89f6e4cbad00b46f9e60
Lucas Henrique <60784320+RKLeonidas@users.noreply.github.com>
2020-03-04 10:19:50 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale (LC_ALL,"Portuguese");
+	int valor1,valor2,mult1,soma1,mult2,soma2,soma3,ano_nasc,valor_final;
+	float birthday_this_year;
+	printf("\tVou adivinhar sua idade\n");
+	printf("Pense em um número de 0 a 9 e insira ele abaixo:\n");
+	scanf("%d",&valor1);
+	mult1=valor1*2;
+	soma1=mult1+5;
+	mult2=soma1*50;
+	printf("Você já fez aniversário esse ano?");
+	scanf("%f",&birthday_this_year);
+	if(birthday_this_year="Não"||birthday_this_year="não"||birthday_this_year="nao");
+	soma2=mult2+1766;
+	if(birthday_this_year="Sim"||birthday_this year="sim");
+	soma2=mult2+1767;
+	printf("Qual o ano que você nasceu?");
+	scanf("%d",&ano_nasc);
+	soma3=soma2-ano_nasc);
+	printf("O resultado será um número de 3 digítos,o primeiro digíto o número que você pensou inicialmente,o segundo dígito a sua idade");
+	printf("%d",soma3);
+	return 0;
+}
+
